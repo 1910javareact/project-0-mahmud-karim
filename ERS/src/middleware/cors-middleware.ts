871c8dd2bb36391601c8f130ec22104a84a905ca
mahmud <mahmudskarim@gmail.com>
2019-12-21 22:10:18 -0500

    fixed cors hopefully

--- ERS/src/middleware/cors-middleware.ts
+++ ERS/src/middleware/cors-middleware.ts
@@ -3,6 +3,8 @@ import { Request } from 'express';
 export function corsFilter(req: Request, res, next) {
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`);
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    //headers.putSingle("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS') {
         res.status(200).send();
